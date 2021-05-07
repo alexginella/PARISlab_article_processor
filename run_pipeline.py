b456affdeb473a39285f0c12466714c30cdf2ef7
Alex Ginella <ginellalex@gmail.com>
2021-05-06 18:35:14 -0700

    linked up table extraction code

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,13 +2,14 @@
 #import generate_tuples
 import clean_article_text
 import structure_sentence
+from table_extraction import process_table_data
 
 def main():
 	
 	filename = "json-files/article0.json"
 	print("processing file:", filename)
 	sentences = clean_article_text.produce_filtered_sentence_data(filename)[0]
-	
+	process_table_data.get_tables()
 	tuples = []
 	for sentence in sentences:
 		sen = structure_sentence.Structure_Sentence(sentence)
